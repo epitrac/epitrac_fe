593b9d2445562fd7154cc3a5d9ad4ddd0fd53643
Darby Smith <newman.darby2@gmail.com>
2023-01-07 13:18:17 -0500

    Update facade to make objects without nil attributes

--- app/facades/articles_facade.rb
+++ app/facades/articles_facade.rb
@@ -1,7 +1,7 @@
 class ArticlesFacade
   def self.articles_on_topic(disease)
     EpitracService.articles_on_topic(disease)[:data].map do |article_info|
-      Article.new(article_info)
+      Article.new(article_info[:attributes])
     end
   end
 end
