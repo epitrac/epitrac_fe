20eea7617913cc27ed1a4e58256fc3bfb0d215c4
Darby Smith <newman.darby2@gmail.com>
2023-01-07 11:29:35 -0500

    Add service for articles

---
+++ app/services/epitrac_service.rb
@@ -0,0 +1,21 @@
+class EpitracService
+  def self.conn
+    Faraday.new(url: "https://epitrac-be.herokuapp.com")
+  end
+
+  def self.get_url(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
+
+  def self.all_states_cases
+    get_url("/api/v1/disease_cases")
+  end
+
+  def self.cases_for_state(state)
+    get_url("/api/v1/disease_cases?state=#{state.upcase}")
+  end
+
+  def self.articles_on_topic(disease)
+    get_url("/api/v1/articles?disease=#{disease}")
+  end
+end
---
+++ spec/services/epitrac_service_spec.rb
@@ -0,0 +1,119 @@
+require 'rails_helper'
+
+RSpec.describe EpitracService do
+  describe 'class methods' do
+    describe '#articles_on_topic' do
+      it 'returns articles for a given disease topic' do
+        articles = EpitracService.articles_on_topic("Anthrax")
+
+        expect(articles).to be_a(Hash)
+        expect(articles[:data]).to be_an(Array)
+        article = articles[:data][0]
+
+        expect(article[:attributes]).to have_key(:id)
+        expect(article[:attributes][:id]).to be_a(String)
+
+        expect(article[:attributes]).to have_key(:author)
+        expect(article[:attributes][:author]).to be_a(String)
+
+        expect(article[:attributes]).to have_key(:title)
+        expect(article[:attributes][:title]).to be_a(String)
+
+        expect(article[:attributes]).to have_key(:year)
+        expect(article[:attributes][:year]).to be_a(String)
+
+        expect(article[:attributes]).to have_key(:date)
+        expect(article[:attributes][:date]).to be_a(String)
+
+        expect(article[:attributes]).to have_key(:isbn_issn)
+        expect(article[:attributes][:isbn_issn]).to be_a(String)
+
+        expect(article[:attributes]).to have_key(:keywords)
+        expect(article[:attributes][:keywords]).to be_a(String)
+
+        expect(article[:attributes]).to have_key(:abstract)
+        expect(article[:attributes][:abstract]).to be_a(String)
+
+        expect(article[:attributes]).to have_key(:url)
+        expect(article[:attributes][:url]).to be_a(String)
+
+        expect(article[:attributes]).to have_key(:doi)
+        expect(article[:attributes][:doi]).to be_a(String)
+      end
+    end
+
+    describe '#all_states_cases' do
+      it 'finds all stats disease cases for the week' do
+        states_cases = EpitracService.all_states_cases
+
+        expect(states_cases).to be_a(Hash)
+        expect(states_cases[:data]).to be_an(Array)
+        state_case = states_cases[:data][0]
+        
+        expect(state_case[:attributes]).to have_key(:state)
+        expect(state_case[:attributes][:state]).to be_a(String)
+
+        expect(state_case[:attributes]).to have_key(:year)
+        expect(state_case[:attributes][:year]).to be_a(String)
+
+        expect(state_case[:attributes]).to have_key(:current_week)
+        expect(state_case[:attributes][:current_week]).to be_a(Integer)
+
+        expect(state_case[:attributes]).to have_key(:disease)
+        expect(state_case[:attributes][:disease]).to be_a(String)
+
+        expect(state_case[:attributes]).to have_key(:cumulative_current)
+        expect(state_case[:attributes][:cumulative_current]).to be_a(Integer)
+
+        expect(state_case[:attributes]).to have_key(:cumulative_last)
+        expect(state_case[:attributes][:cumulative_last]).to be_a(Integer)
+
+        expect(state_case[:attributes]).to have_key(:coordinates)
+        expect(state_case[:attributes][:coordinates]).to be_a(Array)
+
+        expect(state_case[:attributes]).to have_key(:id)
+        expect(state_case[:attributes][:id]).to be_a(String)
+
+        expect(state_case[:attributes]).to have_key(:current_week_cases)
+        expect(state_case[:attributes][:current_week_cases]).to be_a(Integer)
+      end
+    end
+
+    describe '#cases_for_state' do
+      it 'returns only cases for a given state' do
+        state_cases = EpitracService.cases_for_state("georgia")
+
+        expect(state_cases).to be_a(Hash)
+        expect(state_cases[:data]).to be_an(Array)
+        state_case = state_cases[:data][0]
+        
+        expect(state_case[:attributes]).to have_key(:state)
+        expect(state_case[:attributes][:state]).to be_a(String)
+
+        expect(state_case[:attributes]).to have_key(:year)
+        expect(state_case[:attributes][:year]).to be_a(String)
+
+        expect(state_case[:attributes]).to have_key(:current_week)
+        expect(state_case[:attributes][:current_week]).to be_a(Integer)
+
+        expect(state_case[:attributes]).to have_key(:disease)
+        expect(state_case[:attributes][:disease]).to be_a(String)
+
+        expect(state_case[:attributes]).to have_key(:cumulative_current)
+        expect(state_case[:attributes][:cumulative_current]).to be_a(Integer)
+
+        expect(state_case[:attributes]).to have_key(:cumulative_last)
+        expect(state_case[:attributes][:cumulative_last]).to be_a(Integer)
+
+        expect(state_case[:attributes]).to have_key(:coordinates)
+        expect(state_case[:attributes][:coordinates]).to be_a(Array)
+
+        expect(state_case[:attributes]).to have_key(:id)
+        expect(state_case[:attributes][:id]).to be_a(String)
+
+        expect(state_case[:attributes]).to have_key(:current_week_cases)
+        expect(state_case[:attributes][:current_week_cases]).to be_a(Integer)
+      end
+    end
+  end
+end
