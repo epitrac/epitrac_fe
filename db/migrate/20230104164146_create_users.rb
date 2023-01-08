2048756d9f2a663e8f85e731cbf8f1d772f3981e
Darby Smith <newman.darby2@gmail.com>
2023-01-08 14:45:36 -0500

    Refactor users table for oauth

--- db/migrate/20230104164146_create_users.rb
+++ db/migrate/20230104164146_create_users.rb
@@ -1,11 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :uid
       t.string :name
       t.string :email
-      t.string :username
-      t.string :password_digest
-      t.string :state
+      t.string :token
       t.timestamps
     end
   end
