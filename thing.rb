b2c251c7307dd25b37deb17d343de3eda0d43f8d
Madeline Mauser <madeline.mauser@gmail.com>
2023-01-08 23:25:07 -0700

    Add pins for multiple states

---
+++ thing.rb
@@ -0,0 +1,12 @@
+@states_array = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
+thing = @states_array.each do |state|
+print ".map button.btn.pin.#{state.downcase.gsub(' ','_')}, .map a.btn.pin.#{state.downcase.gsub(' ','_')} {
+  left: 32%;
+  top: 43%;
+}
+
+  "
+end
+
+return thing
