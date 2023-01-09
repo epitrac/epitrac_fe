464cf0e466909b06c2117a93529a96c3b91ff7bd
Madeline Mauser <madeline.mauser@gmail.com>
2023-01-08 21:02:51 -0700

    Fix minor merge error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-
-  def new
-    @states_array = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
-  end
-
-=======
->>>>>>> cec582048bbdc842c97dcb93bb1ecd0351719f02
   def show
     @user = User.find(session[:user_id])
   end
