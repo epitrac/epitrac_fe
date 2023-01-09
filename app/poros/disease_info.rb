5d8a29fed3441268f9821057c8147bc89246ed99
Darby Smith <newman.darby2@gmail.com>
2023-01-09 16:22:39 -0500

    Refactor with short name

--- app/poros/disease_info.rb
+++ app/poros/disease_info.rb
@@ -1,11 +1,13 @@
 class DiseaseInfo
   attr_reader :disease,
               :information,
-              :link
+              :link, 
+              :short_name
 
   def initialize(attributes)
     @disease = attributes[:disease]
     @information = attributes[:information]
     @link = attributes[:link]
+    @short_name = attributes[:short_name]
   end
 end
