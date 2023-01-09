810a7d052508394f0a3b85ac1b9b69f1008802c3
Darby Smith <newman.darby2@gmail.com>
2023-01-08 20:29:39 -0500

    Update testing for oauth

--- spec/features/landing/landing_spec.rb
+++ spec/features/landing/landing_spec.rb
@@ -1,31 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe 'application landing page' do
+  describe 'user logged into application' do
     before :each do
-        @user_1 = User.create!(name: "Spongebob", email: "Sponge.bob@underthesea.com", username: "grillmaster1", password: "gary123", password_confirmation: "gary123", state: "Colorado")
-        visit root_path
+      visit root_path
+      
+      expect(page).to have_button("Login with Google")
+      click_on "Login with Google"
+      
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
     end
 
     it "has application name " do
-        expect(page).to have_content("EpiTrac")
+      expect(page).to have_content("EpiTrac")
     end
 
+    it "has a link to about page " do
+      expect(page).to have_link("About")
+      click_on "About"
+      
+      expect(current_path).to eq("/about")
+    end
+  end
 
+  describe 'user is not logged into application' do
     it  "has a create user button " do
-        expect(page).to have_button("Create User")
-        click_on "Create User"
-        expect(current_path).to eq("/users/new")
-    end
+      visit root_path
 
+      expect(page).to have_button("Create User with Google")
+      click_on "Create User with Google"
+      
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+      
+      expect(current_path).to eq(root_path)
+    end
+    
     it  "has a login button " do
-        expect(page).to have_button("Login")
-        click_on "Login"
-        expect(current_path).to eq("/login")
-    end
+      visit root_path
+      
+      expect(page).to have_button("Login with Google")
+      click_on "Login with Google"
+      
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
 
-     it "has a link to about page " do
-        expect(page).to have_link("About")
-        click_on "About"
-        expect(current_path).to eq("/about")
+      expect(current_path).to eq(root_path)
     end
+  end 
 end
