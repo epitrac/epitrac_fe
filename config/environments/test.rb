8d813ca211be4c6794a38eaa9f85aa6cfc609128
Darby Smith <newman.darby2@gmail.com>
2023-01-08 20:26:35 -0500

    Add test configuration for omniauth

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,4 +43,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      'uid' => '1234567',
+      'credentials' => {'token' => '987654321'},
+      'info' => {
+        'name' => 'Spongebob Squarepants',
+        'email' => 'spongebob@gmail.com'
+      }
+    })
 end
+
