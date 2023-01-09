ea5b489ed3ce8ad76f91f53444b7d6e90bd1ec7b
Darby Smith <newman.darby2@gmail.com>
2023-01-08 20:28:24 -0500

    Update migration/schema

--- db/migrate/20230104164146_create_users.rb
+++ db/migrate/20230104164146_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :email
       t.string :token
+      t.string :state, default: nil
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_01_04_164146) do
     t.string "name"
     t.string "email"
     t.string "token"
+    t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
