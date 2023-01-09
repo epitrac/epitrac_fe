3a414b8a408a995708f72d8ff7e7d844855a88c1
Darby Smith <newman.darby2@gmail.com>
2023-01-08 20:25:17 -0500

    Refactor sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,23 @@
 class SessionsController < ApplicationController
   def create
-    auth_hash = request.env['omniauth.auth']
-    uid = auth_hash['uid']
-    email = auth_hash['info']['email']
-    name = auth_hash['info']['name']
-    token = auth_hash['credentials']['token']
-
-    user = User.find_or_create_by(uid: uid)
-    user.update(email: email, name: name, token: token)    
+    user = User.find_or_create_by(uid: user_params[:uid])
+    user.update(user_params)    
 
     session[:user_id] = user.id
 
     redirect_to root_path
   end
-end
 
-# /auth/google_oauth2
+  private
+
+  def auth_hash
+    auth_hash = request.env['omniauth.auth']
+  end
+
+  def user_params
+    {uid: auth_hash['uid'],
+    email: auth_hash['info']['email'],
+    name: auth_hash['info']['name'],
+    token: auth_hash['credentials']['token']}
+  end
+end
