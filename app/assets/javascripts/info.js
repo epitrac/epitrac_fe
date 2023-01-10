953bd3016f173aa429519a74d36749a7a2185736
Darby Smith <newman.darby2@gmail.com>
2023-01-10 15:06:48 -0500

    Add diseases to home page

--- app/assets/javascripts/info.js
+++ app/assets/javascripts/info.js
@@ -1,3 +1,13 @@
-function changeInfo(info) {
-  document.getElementById("info").innerHTML = info;
+async function changeInfo(state) {
+  let response = await fetch(`http://localhost:5000/api/v1/disease_cases?state=${state}`);
+  console.log(response.json)
+  let data = await response.json();
+  console.log("1", data)
+  console.log("2", data.data)
+  // let data = JSON.parse(info);
+  document.getElementById("info").innerHTML = data.data.map(disease => "<br>" + disease.attributes.disease + ": " + disease.attributes.current_week_cases);
+
+}
+function resetInfo(data) {
+  document.getElementById("info").innerHTML = data
 }
