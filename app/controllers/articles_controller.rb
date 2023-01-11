9e79be0161e234523302c7a8deb91dcd4a9f61f0
Emily Port <emilylport@gmail.com>
2023-01-10 19:56:29 -0700

    fix articles by disease

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
   def index 
-    @disease = DiseaseInfoFacade.disease_info(params[:disease]).disease
-    @articles = ArticlesFacade.articles_on_topic(@disease)
+    @articles = ArticlesFacade.articles_on_topic(params[:disease])
   end
 end
