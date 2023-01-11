834bccb75d3c7b336cb1f7c5c75d1b626763b5df
Darby Smith <newman.darby2@gmail.com>
2023-01-11 18:32:38 -0500

    Update sad path tests for users

--- spec/features/users/destroy_spec.rb
+++ spec/features/users/destroy_spec.rb
@@ -1,23 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'delete user', :vcr do
-  before :each do
-    ActiveRecord::Base.connection.reset_pk_sequence!('users')
-    visit root_path
+  describe 'user is logged in' do
+    before :each do
+      ActiveRecord::Base.connection.reset_pk_sequence!('users')
+      visit root_path
+        
+      expect(page).to have_button("Login with Google")
+      click_on "Login with Google"
       
-    expect(page).to have_button("Login with Google")
-    click_on "Login with Google"
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    end
+
+    it "can delete a user " do
+
+      visit "/dashboard"
+
+      click_link "Delete Account"
+      
+      expect(current_path).to eq("/")
+    end
     
-    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    it 'cannot go to a dashboard that has been deleted' do
+      visit '/dashboard'
+
+      click_link "Delete Account"
+      
+      expect(current_path).to eq("/")
+      
+      visit '/dashboard'
+      
+      expect(current_path).to eq("/")
+      expect(page).to have_content("Please login to view your dashboard.")
+    end
   end
-
-  it "can delete a user " do
-
-    visit "/dashboard"
-
-    click_link "Delete Account"
-    
-    expect(current_path).to eq("/")
-  end
-
 end
--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -1,27 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'user edit state', :vcr do
-  before :each do
-    ActiveRecord::Base.connection.reset_pk_sequence!('users')
+  describe 'user is logged in' do
+    before :each do
+      ActiveRecord::Base.connection.reset_pk_sequence!('users')
 
-    visit root_path
+      visit root_path
+        
+      expect(page).to have_button("Login with Google")
+      click_on "Login with Google"
       
-    expect(page).to have_button("Login with Google")
-    click_on "Login with Google"
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    end
     
-    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    it 'lets a user add or update the state' do
+
+      visit '/dashboard/edit'
+
+      select 'Georgia', from: :state
+      click_on "Submit"
+
+      expect(current_path).to eq('/dashboard')
+    end
   end
-  
-  it 'lets a user add or update the state' do
 
-    visit '/dashboard/edit'
+  describe 'user is not logged in' do
+    it 'a user cannot acces a profile edit page' do
+      visit '/dashboard/edit'
 
-    select 'Georgia', from: :state
-    click_on "Submit"
-
-
-    expect(current_path).to eq('/dashboard')
-
-  
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("Please login to edit your state.")
+    end
   end
 end
