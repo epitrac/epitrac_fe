91934347fe6e967900afca03d0bce5cbbe1246ed
Emily Port <emilylport@gmail.com>
2023-01-11 14:25:33 -0700

    testing finally passes

--- spec/features/users/destroy_spec.rb
+++ spec/features/users/destroy_spec.rb
@@ -2,19 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'delete user', :vcr do
   before :each do
+    ActiveRecord::Base.connection.reset_pk_sequence!('users')
     visit root_path
       
     expect(page).to have_button("Login with Google")
     click_on "Login with Google"
     
     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    # VCR.insert_cassette('delete a user')
+
   end
 
   it "can delete a user " do
+    # VCR.insert_cassette('delete a user')
+
+    # VCR.eject_cassette
+    # VCR.insert_cassette('delete a user')
+    # expect(page).to have_link(“Link Name”, href: expected_path)
     visit "/dashboard"
-    
+    # VCR.eject_cassette
+
     click_link "Delete Account"
     
     expect(current_path).to eq("/")
+    # VCR.eject_cassette
   end
+
 end
--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -2,20 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'user edit state', :vcr do
   before :each do
+    ActiveRecord::Base.connection.reset_pk_sequence!('users')
+
     visit root_path
       
     expect(page).to have_button("Login with Google")
     click_on "Login with Google"
     
     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    # VCR.eject_cassette
+    # VCR.insert_cassette('user can add or update state')
+  
   end
   
   it 'lets a user add or update the state' do
+
     visit '/dashboard/edit'
 
     select 'Georgia', from: :state
     click_on "Submit"
 
+
     expect(current_path).to eq('/dashboard')
+
+  
   end
 end
